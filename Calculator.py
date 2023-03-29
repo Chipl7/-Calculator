d9f4f898844e22fa17520bd8221ed4a35796b107
Artem Dubrovin <artem.chipl@gmail.com>
2023-03-29 14:29:14 +0700

    .
    .

--- Calculator.py
+++ Calculator.py
@@ -1,7 +1,35 @@
-print ('Hellow world')
-
-n = int(input('Введите первое число: '))
-i = int(input('Введите второе число: '))
-while i < n:
-    print(i)
-    i += 1
+from math import sqrt
+print('\tКалькулятор')
+a1 = int(input('Введите первое число: '))
+a2 = int(input('Введите второе число: '))
+a3 = input('Введите оператор: ')
+if a3 == '+':
+    print(f'{a1} + {a2} = {a1 + a2}')
+elif a3 == '-':
+    print(f'{a1} - {a2} = {a1 - a2}')
+elif a3 == 'корень':
+    print(sqrt(a1),'\t',sqrt(a2))
+elif a3 == '*':
+    print(f'{a1} * {a2} = {a1 * a2}')
+elif a3 == '**':
+    print(f'{a1} ** {a2} = {a1 ** a2}')
+elif a3 == '/' and a2 != 0:
+    print(f'{a1} / {a2} = {a1 / a2}')
+elif a3 == '/' and a2 == 0:
+    print('На ноль делить нельзя!')
+    a2 = int(input('Введите второе число заново: '))
+    print(f'{a1} / {a2} = {a1 / a2}')
+elif a3 == '//' and a2 != 0:
+    print(f'{a1} // {a2} = {a1 // a2}')
+elif a3 == '//' and a2 == 0:
+    print('На ноль делить нельзя!')
+    a2 = int(input('Введите второе число заново: '))
+    print(f'{a1} // {a2} = {a1 // a2}')
+elif a3 == '%' and a2 != 0:
+    print(f'{a1} % {a2} = {a1 % a2}')
+elif a3 == '%' and a2 == 0:
+    print('На ноль делить нельзя!')
+    a2 = int(input('Введите второе число заново: '))
+    print(f'{a1} % {a2} = {a1 % a2}')
+else:
+    print('Я тебя не понимаю')
